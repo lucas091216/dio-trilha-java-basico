007504bb0e2c2d109fe8c3a426731b40e9a9e33a
lucas091216 <lucas_vital2011@hotmail.com>
2024-05-11 12:12:43 -0300

    Projeto  conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        Scanner teclado = new Scanner(System.in);
+
+
+        Integer numero;
+        String agencia;
+        String NomeCliente;
+        Float saldo;
+
+        System.out.println("Numero: (ENTER para confirmar)");
+        numero = teclado.nextInt();
+
+        System.out.println("Agencia: (ENTER para confirmar)");
+        agencia = teclado.next();
+
+        System.out.println("Nome do cliente: (ENTER para confirmar)");
+        NomeCliente = teclado.next();
+
+        System.out.println("Saldo da conta: (ENTER para confirmar)");
+        saldo=teclado.nextFloat();
+
+
+        System.out.println("Ola " + NomeCliente+ ", obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + ", conta "+ numero + "e seu saldo " + saldo + "já está disponivel para saque");
+
+
+
+
+
+
     }
 }
